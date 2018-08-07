4494517fb182194f088c3e83390404646ed0aa39
Maple24 <uncharted_24@foxmail.com>
2018-08-07 10:37:58 +0800

    sort functions written

---
+++ bubble.py
@@ -0,0 +1,10 @@
+def bubble(arr):
+    for i in range(0,len(arr)-1):
+        for j in range(0,len(arr)-i-1):
+            if(arr[j] > arr[j+1]):
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+
+if __name__ == '__main__':
+    arr = [3,5,4,2,1,7,6]
+    bubble(arr)
+    print(arr)
