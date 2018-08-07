c18b4cf8d26b328a1b1334e2354acc3258257236
Maple24 <uncharted_24@foxmail.com>
2018-08-07 16:28:31 +0800

    update quickSort

--- quickSort.py
+++ quickSort.py
@@ -1,12 +1,13 @@
 def quickSort(L, low, high):
     i = low
     j = high
-    key = L[i]
     if i >= j:
         return L
+    key = L[i]
     while i < j:
         while i < j and L[j] >= key:
             j = j - 1
+        L[i] = L[j]
         while i < j and L[i] <= key:
             i = i + 1
         L[j] = L[i]
@@ -17,7 +18,7 @@ def quickSort(L, low, high):
 
 
 if __name__ == '__main__':
-    L = [6,3,1,4,5]
+    L = [6,3,2,1,4,5]
     quickSort(L, 0, len(L)-1)
     print(L)
     
