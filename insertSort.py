96d23742542c8cfd16a153f8dffc2226518c7ae4
Maple24 <uncharted_24@foxmail.com>
2018-08-08 14:19:09 +0800

    modify insertSort.py

--- insertSort.py
+++ insertSort.py
@@ -1,12 +1,12 @@
 def insertSort(L):
-    for i in range(0,len(L)):
+    for i in range(1,len(L)):
         j = i
         while(j>0 and L[j] < L[j-1]):
             L[j],L[j-1] = L[j-1],L[j]
             j = j-1
-        print("The "+ str(i+1) + " round: "+ str(L))
+        print("The "+ str(i) + " round: "+ str(L))
     return L
 
 if __name__ == '__main__':
-    L = [5,3,2,4,1]
+    L = [4,5,2,3,1]
     insertSort(L)
